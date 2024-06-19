b75000d90ea2e0ad5580ab25c08552b4944dd01b
Brayan Casallas <beto980730@gmail.com>
2024-06-19 17:45:24 -0500

    Actualizacion JS

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-alert("Hola Mundo");
+let numeroSecreto = 6;
 let numeroUsuario = prompt("Elije un numero:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+  alert("Correcto");
+  console.log("Correcto");
+} else {
+  alert("incorrecto");
+  console.log("incorrecto");
+}
