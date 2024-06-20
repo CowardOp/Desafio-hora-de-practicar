adde5d941619ff9330f0f1a7e574bffdb28a168e
Brayan Casallas <beto980730@gmail.com>
2024-06-20 12:01:03 -0500

    Actualiza el JS

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 //variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Ingresa un numero de 1 a 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Ingresa un numero de 1 a 10 por favor:");
 
-console.log(numeroUsuario);
+  console.log(numeroUsuario);
 
-/* 
-Este codigo realiza la comparacion
-*/
-
-if (numeroUsuario == numeroSecreto) {
-  //Acertaste
-  alert(`Asertaste, el numero es: ${numeroUsuario}`);
-  console.log("Correcto");
-} else {
-  //Incorrecto
-  alert("Incorrecto");
-  console.log("Incorrecto");
+  if (numeroUsuario == numeroSecreto) {
+    //Acertaste
+    alert(`Asertaste, el numero es:  ${numeroUsuario}`);
+    console.log("Correcto");
+    intentos = 0;
+    break;
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor" + `lleva ${intentos} intentos`);
+    } else {
+      alert("El numero secreto es mayor" + `lleva ${intentos} intentos`);
+    }
+    intentos++;
+    console.log("Incorrecto");
+  }
 }
