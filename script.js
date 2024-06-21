1aac718efe7d4b7e9d8fdfeb6e1ea0a2300e1237
Brayan Casallas <beto980730@gmail.com>
2024-06-21 16:16:52 -0500

    Actualiza ultimo JS

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 //variables
-let numeroSecreto = 6;
+let numeroMaximo = 15;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraIntentos = "intento";
+let maxIntentos = 10;
 
+console.log(numeroSecreto);
+
+//bucle
 while (numeroUsuario != numeroSecreto) {
-  numeroUsuario = prompt("Ingresa un numero de 1 a 10 por favor:");
+  numeroUsuario = prompt(`Ingresa un numero de 1 a ${numeroMaximo} por favor:`);
 
   console.log(numeroUsuario);
 
   if (numeroUsuario == numeroSecreto) {
     //Acertaste
     alert(
-      `Asertaste, el numero es:  ${numeroUsuario}, lo lograste en ${intentos} ${palabraIntentos}`
+      `Asertaste, el numero es:  ${numeroUsuario}, lo lograste en ${intentos} ${
+        intentos == 1 ? "intento" : "intentos"
+      }`
     );
     console.log("Correcto");
-    intentos = 0;
-    break;
   } else {
     if (numeroUsuario > numeroSecreto) {
-      alert("El numero secreto es menor" + `lleva ${intentos} intentos`);
+      alert(
+        "El numero secreto es menor " +
+          `lleva ${intentos} ${intentos == 1 ? "intento" : "intentos"}`
+      );
     } else {
-      alert("El numero secreto es mayor" + `lleva ${intentos} intentos`);
+      alert(
+        "El numero secreto es mayor " +
+          `lleva ${intentos} ${intentos == 1 ? "intento" : "intentos"}`
+      );
     }
     intentos++;
-    palabraIntentos = "intentos";
-    console.log("Incorrecto");
+    console.log(intentos);
+
+    if (intentos > maxIntentos) {
+      alert(`Llegaste al numero maximo de ${maxIntentos} intentos`);
+      break;
+    }
   }
 }
